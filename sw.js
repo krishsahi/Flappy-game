173bf273877c941c8262c29d7ef4a03c73159edf
krishsahi <krishsahu2131@gmail.com>
2026-02-16 13:06:13 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "bookflappy-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./face.png",
+  "./img/books.png",
+  "./sounds/bg.mp3",
+  "./sounds/hit.mp3",
+  "./sounds/fall.mp3",
+  "./sounds/win.mp3"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
